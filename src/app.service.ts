b768fe3bc10de8fb20066105c8b7bd016e291cd7
JCGM <juliancamilo0210@gmail.com>
2024-07-11 15:37:09 -0500

    add error try again

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,7 @@ export class AppService {
   getHello(): string {
     const sayHello: string = 'hello';
     const sayOtherThing: string = 'Other Thing';
+    const sayOtherThing2: string = 'Other Thing 2';
     return 'Hello World!';
   }
 }
